2e9be564f943900e1d8c894ced83a205d13e068c
cammmbusss <camimalvin@gmail.com>
2026-06-03 20:53:37 -0300

    Add files via upload

---
+++ PP.hs
@@ -0,0 +1,10 @@
+{- TAREA DE PROGRAMACIÓN FUNCIONAL 2026 -}
+{- PRETTY-PRINTING -}
+module PP where
+
+import AST
+
+-- Pretty-printing de un programa
+-- El comportamiento de la función se especifica en la letra de la Tarea.
+ppProg :: Prog -> String
+ppProg = undefined
